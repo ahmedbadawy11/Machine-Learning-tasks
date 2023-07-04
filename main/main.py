c557161aea7dd923d7d89a35bd569dac8c462a72
Esraa fayad <79775750+esraafayad12@users.noreply.github.com>
2023-07-04 15:01:23 +0300

    Added DR

--- main/main.py
+++ main/main.py
@@ -42,6 +42,63 @@ MultinomialNB_predictions=Naive_Bayes_Models(GaussianNB(),X_train,y_train,X_test
 
 y_predict=modeling.bernoulliNB()
 
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+from preprocessing.dimensionality_reduction import DR
+from modeling.knn import Knn
+from modeling.nb import NaiveBayes
+
+dr = DR()
+n_cmops = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+f1_score_pca_knn = []
+f1_score_autoencoder_knn = []
+f1_score_pca_nb = []
+f1_score_autoencoder_nb = []
+
+for i in n_cmops:
+    transformed_pca_train_data = dr.create_pca_model(X_train, i, 42)
+    transformed_pca_test_data = dr.create_pca_model(X_test, i, 42)
+    transformed_autoencoder_train_data = dr.create_autoencoder_model(X_train, i)
+    transformed_autoencoder_test_data = dr.create_autoencoder_model(X_test, i)
+    knn_model_pca = Knn(transformed_pca_train_data, y_train, transformed_pca_test_data)
+    knn_model_autoencoder = Knn(transformed_autoencoder_train_data, y_train, transformed_pca_test_data)
+    naive_bays_model_pca = NaiveBayes(transformed_pca_train_data, y_train, transformed_autoencoder_test_data)
+    naive_bays_model_autoencoder = NaiveBayes(transformed_autoencoder_train_data, y_train, transformed_autoencoder_test_data)
+
+    y_pred_knn_pca = knn_model_pca.knn(2)
+    classification_report_knn_pca = classification_report(y_test, y_pred_knn_pca, output_dict=True)
+    f1_score_pca_knn.append(classification_report_knn_pca["macro avg"]["f1-score"])
+
+    y_pred_knn_autoencoder = knn_model_autoencoder.knn(2)
+    classification_report_knn_autoencoder = classification_report(y_test, y_pred_knn_autoencoder, output_dict=True)
+    f1_score_autoencoder_knn.append(classification_report_knn_autoencoder["macro avg"]["f1-score"])
+
+    y_pred_nb_pca = naive_bays_model_pca.gaussianNB()
+    classification_report_nb_pca = classification_report(y_test, y_pred_nb_pca, output_dict=True)
+    f1_score_pca_nb.append(classification_report_nb_pca["macro avg"]["f1-score"])
+
+    y_pred_nb_autoencoder = naive_bays_model_autoencoder.gaussianNB()
+    classification_report_nb_autoencoder = classification_report(y_test, y_pred_nb_autoencoder, output_dict=True)
+    f1_score_autoencoder_nb.append(classification_report_nb_autoencoder["macro avg"]["f1-score"])
+
+    print("PCA - KNN - Classification Report (n_components = {})".format(i))
+    print(classification_report(y_test, y_pred_knn_pca))
+    print("Autoencoder - KNN - Classification Report (n_components = {})".format(i))
+    print(classification_report(y_test, y_pred_knn_autoencoder))
+    print("PCA - Naive Bayes - Classification Report (n_components = {})".format(i))
+    print(classification_report(y_test, y_pred_nb_pca))
+    print("Autoencoder - Naive Bayes - Classification Report (n_components = {})".format(i))
+    print(classification_report(y_test, y_pred_nb_autoencoder))
+
+plt.plot(n_cmops, f1_score_pca_knn, label="PCA - KNN")
+plt.plot(n_cmops, f1_score_autoencoder_knn, label="Autoencoder - KNN")
+plt.plot(n_cmops, f1_score_pca_nb, label="PCA - Naive Bayes")
+plt.plot(n_cmops, f1_score_autoencoder_nb, label="Autoencoder - Naive Bayes")
+plt.xlabel("Number of components")
+plt.ylabel("F1 score")
+plt.legend()
+plt.show()
+
 # ---------- Visualization--------------
 
 # ---------- Evaluation--------------
@@ -52,4 +109,5 @@ evaluation_Ber = Evaluation(y_test, y_predict)
 evaluation_NB.conf_matrix("Multinomial Naive Bayes Classifiers for Part 2 (A)")
 evaluation_KNN.conf_matrix("KNN")
 evaluation_Ber.conf_matrix("evaluation_Ber")
+
 
--- main/preprocessing/dimensionality_reduction.py
+++ main/preprocessing/dimensionality_reduction.py
@@ -1,10 +1,37 @@
 from sklearn.decomposition import PCA
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
 
 
 class DR:
     def __init__(self):
-        ...
+       pass
 
-    def pca(self, data, n_components=0, random_state=0):
+    def create_pca_model(self, data, n_components=0, random_state=0):
         pca = PCA(n_components=n_components, random_state=random_state)
         return pca.fit_transform(data)
+
+    def create_autoencoder_model(self,data,outputs):
+        """
+        :param data:
+        :param outputs:
+        :return:
+        Create an autoencoder model
+        """
+        input_layer = Input(shape=(data.shape[1],))
+        encoder = Dense(10, activation="relu")(input_layer)
+        encoder = Dense(7, activation="relu")(encoder)
+        encoder = Dense(5, activation="relu")(encoder)
+        encoder = Dense(3, activation="relu")(encoder)
+        encoder = Dense(outputs, activation="relu")(encoder)
+        decoder = Dense(3, activation="relu")(encoder)
+        decoder = Dense(5, activation="relu")(decoder)
+        decoder = Dense(7, activation="relu")(decoder)
+        decoder = Dense(10, activation="relu")(decoder)
+        decoder = Dense(data.shape[1], activation="sigmoid")(decoder)
+        autoencoder = Model(inputs=input_layer, outputs=decoder)
+        autoencoder.compile(optimizer="adam", loss="mse")
+        autoencoder.fit(data, data, epochs=10, batch_size=32, verbose=0)
+        encoder = Model(inputs=input_layer, outputs=encoder)
+        return encoder.predict(data)
